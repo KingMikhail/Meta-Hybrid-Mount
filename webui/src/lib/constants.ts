df910fbe112d8862b2165ac3d0d9ff425908860e
Mikhail Andrei <andreimikhail1987@gmail.com>
2026-02-14 13:52:19 +0800

    Update constants.ts

--- webui/src/lib/constants.ts
+++ webui/src/lib/constants.ts
@@ -9,7 +9,7 @@ import type { AppConfig } from "./types";
 export const DEFAULT_CONFIG: AppConfig = {
   moduledir: "/data/adb/modules",
   mountsource: "KSU",
-  logfile: RUST_PATHS.DAEMON_LOG || "/data/adb/hybrid-mount/daemon.log",
+  logfile: RUST_PATHS.DAEMON_LOG || "/data/adb/Hybrid-Mount/daemon.log",
   partitions: [],
   disable_umount: false,
   allow_umount_coexistence: false,
@@ -18,7 +18,7 @@ export const DEFAULT_CONFIG: AppConfig = {
 
 export const PATHS = {
   ...RUST_PATHS,
-  BINARY: "/data/adb/modules/hybrid-mount/hybrid-mount",
+  BINARY: "/data/adb/modules/Hybrid-Mount/Hybrid-Mount",
 };
 
 export const DEFAULT_SEED = "#6750A4";
