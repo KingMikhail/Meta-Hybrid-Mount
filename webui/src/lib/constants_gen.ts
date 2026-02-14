e85e1bacd46416d81fdf832aa174929c47fa223d
Mikhail Andrei <andreimikhail1987@gmail.com>
2026-02-14 13:55:55 +0800

    Update constants_gen.ts

--- webui/src/lib/constants_gen.ts
+++ webui/src/lib/constants_gen.ts
@@ -1,11 +1,11 @@
 
 export const APP_VERSION = "v3.0.1-5-g950a83e-dirty";
 export const RUST_PATHS = {
-  CONFIG: "/data/adb/meta-hybrid/config.toml",
-  MODE_CONFIG: "/data/adb/meta-hybrid/module_mode.conf",
-  IMAGE_MNT: "/data/adb/meta-hybrid/mnt",
-  DAEMON_STATE: "/data/adb/meta-hybrid/run/daemon_state.json",
-  DAEMON_LOG: "/data/adb/meta-hybrid/daemon.log",
+  CONFIG: "/data/adb/Meta-Hybrid/config.toml",
+  MODE_CONFIG: "/data/adb/Meta-Hybrid/module_mode.conf",
+  IMAGE_MNT: "/data/adb/meta-Hybrid/mnt",
+  DAEMON_STATE: "/data/adb/Meta-Hybrid/run/daemon_state.json",
+  DAEMON_LOG: "/data/adb/Meta-Hybrid/daemon.log",
 } as const;
 export const BUILTIN_PARTITIONS = ["system", "vendor", "product", "system_ext", "odm", "oem", "apex"] as const;
 export const IS_RELEASE = false;
